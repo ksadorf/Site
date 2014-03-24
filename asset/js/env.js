4ce945fca6334a0905d8e1eb056bee67a9200424
ksad <noe.gaumont@gmail.com>
2014-03-24 19:16:40 +0100

    Old version of my website

---
+++ asset/js/env.js
@@ -0,0 +1,25 @@
+addNavbar=function(){
+	var newdiv = document.getElementById("menu-wrapper");
+	newdiv.innerHTML='\
+	<div class="navbar navbar-inverse navbar-fixed-top">\
+		  <div class="navbar-inner">      \
+			<div class="container">    \
+			  <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">  \
+				<span class="icon-bar"></span>\
+				<span class="icon-bar"></span>\
+				<span class="icon-bar"></span>\
+			  </button>                                      \
+			  <a class="brand" href="index.html">Gaumont Noé</a>      \
+			  <div class="nav-collapse collapse">              \
+				<ul class="nav">                                  \
+				  <li><a href="Parcours.html">Parcours académique</a></li>        \
+				  <li><a href="Experience.html">Expériences professionnelles</a></li>        \
+				  <li><a href="Projets.html">Projets</a></li>        \
+				  <li><a href="http://www.ngaumont.fr/owncloud/" target="_blank">Cloud privé</a></li>        \
+				</ul>                                           \
+			  </div><!--/.nav-collapse -->                     \
+			</div>                                         \
+		  </div>                                    \
+	</div>\
+	';
+}
